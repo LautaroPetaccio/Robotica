a67fa2920eb1101152e17a40b33007dd5ae81467
Lautaro Petaccio <lausuper@gmail.com>
2016-03-18 12:18:10 -0300

    Changed order in robotica_motor

--- libraries/blockly/generators/javascript/robotica.js
+++ libraries/blockly/generators/javascript/robotica.js
@@ -3,12 +3,11 @@
 goog.require('Blockly.JavaScript');
 
 Blockly.JavaScript['robotica_motor'] = function(block) {
-  // Search the text for a substring.
   var value_leftwheel = Blockly.JavaScript.valueToCode(block, 'leftWheel', Blockly.JavaScript.ORDER_ATOMIC);
   var value_rightwheel = Blockly.JavaScript.valueToCode(block, 'rightWheel', Blockly.JavaScript.ORDER_ATOMIC);
   var value_motorpower = Blockly.JavaScript.valueToCode(block, 'motorPower', Blockly.JavaScript.ORDER_ATOMIC);
   var code = "motor(" + value_leftwheel + ' , ' + value_rightwheel + ' , ' + value_motorpower + ');\n';
-  return [code, Blockly.JavaScript.ORDER_ATOMIC];
+  return [code, Blockly.JavaScript.ORDER_NONE];
 };
 
 Blockly.JavaScript['robotica_sensor'] = function(block) {
