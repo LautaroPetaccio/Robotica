cdd65befa0f214172c7e67aea01d623cd6161426
Lautaro Petaccio <lausuper@gmail.com>
2016-03-19 10:43:27 -0300

    Added the code execution state to the gmae

--- game/js/game.js
+++ game/js/game.js
@@ -12,7 +12,22 @@ var game = {
 
 
   // Run on page load.
+
   "onload" : function () {
+    /* 
+      0 => running
+      1 => paused
+      2 => finished
+    */
+    me.excecution = {
+      state : 2,
+      pause : function() { this.state = 1; },
+      run : function() { this.state = 0; },
+      finish : function() { this.state = 2; },
+      isPaused : function() { return this.state == 1; },
+      hasFinished : function() { return this.state == 2; },
+    };
+
     // Initialize the video.
     if (!me.video.init(720, 640, {wrapper : "screen", scale : "auto", scaleMethod : "fit", antiAlias: true})) {
       alert("Your browser does not support HTML5 canvas.");
