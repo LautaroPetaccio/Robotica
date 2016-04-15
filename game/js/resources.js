baf8eaa310feb221797c4d5cd73d4755c1e9d506
Lautaro Petaccio <lausuper@gmail.com>
2016-04-15 13:45:03 -0300

    Game cleanup and first box added to the map to test collisions

--- game/js/resources.js
+++ game/js/resources.js
@@ -1,32 +1,15 @@
 game.resources = [
-	/**
-	 * Graphics.
-	 */
-	// the main player spritesheet
+	/* Graphics */
+
+	/* Robot sprite */
 	{name: "robot50",     type:"image",	src: "game/data/img/sprite/robot50.png"},
-	// game font
+	/* Game font */
 	{name: "32x32_font",          type:"image",	src: "game/data/img/font/32x32_font.png"},
-	// title screen
-	// {name: "title_screen",        type:"image",	src: "data/img/gui/title_screen.png"},
-	// our level tileset
+	/* Map sprites */
 	{name: "m2",  type:"image",	src: "game/data/img/map/m2.png"},
+	{name: "box",  type:"image",	src: "game/data/img/sprite/box.png"},
 	
-	/* 
-	 * Maps. 
- 	 */
-	// {name: "area01",              type: "tmx",	src: "data/map/area01.tmx"},
-	// {name: "area02",              type: "tmx",	src: "data/map/area02.tmx"},
+	/*  Maps */
 	{name: "area03",              type: "tmx",	src: "game/data/map/area03.tmx"},
 
-	/* 
-	 * Background music. 
-	 */	
-	// {name: "dst-inertexponent", type: "audio", src: "data/bgm/"},
-	
-	/* 
-	 * Sound effects. 
-	 */
-	// {name: "cling", type: "audio", src: "data/sfx/"},
-	// {name: "stomp", type: "audio", src: "data/sfx/"},
-	// {name: "jump",  type: "audio", src: "data/sfx/"}
 ];
