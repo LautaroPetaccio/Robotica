dbdebf99b8670bf84eef72f6bbcca3f9232b7f4c
Emilio Almansi <ealmansi@gmail.com>
2016-06-08 19:15:22 -0300

    Arreglo el bug del issue 58. Fix #58.

--- src/app/home/home.js
+++ src/app/home/home.js
@@ -35,7 +35,6 @@ this.Home = (function() {
 
     homeNavbarCollapseElement.on('shown.bs.collapse hidden.bs.collapse', HomeBlockly.forceBlocklySvgResize);
     homeSimulatorWrapperElement.click(module.toggleLayoutMode);
-    window.addEventListener('resize', module.setInitialLayoutMode, false);
     window.addEventListener('orientationchange', module.resetDraggablePosition, false);
 
     $("#maps-modal .media").click(module.onMapSelectionClick);
