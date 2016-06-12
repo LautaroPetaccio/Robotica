396e31df6337bfeafacc31dfa4e59b8213043b65
Emilio Almansi <ealmansi@gmail.com>
2016-06-11 21:13:45 -0300

    Ordeno por estado: 0, 1, 2.

--- src/game/js/game.js
+++ src/game/js/game.js
@@ -12,12 +12,12 @@ var game = {
     */
     me.execution = {
       state : 2,
-      pause : function() { this.state = 1; },
       run : function() { this.state = 0; },
+      pause : function() { this.state = 1; },
       finish : function() { this.state = 2; },
-      isPaused : function() { return this.state == 1; },
       isRunning : function() { return this.state == 0; },
-      hasFinished : function() { return this.state == 2; },
+      isPaused : function() { return this.state == 1; },
+      hasFinished : function() { return this.state == 2; }
     };
 
     me.game.changeMap = function(mapName) {
