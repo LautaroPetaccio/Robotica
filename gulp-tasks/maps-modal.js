a613ac263e0a32c8fdfa04294d27196fd01e0195
Lautaro Petaccio <lausuper@gmail.com>
2016-08-05 15:53:39 -0300

    Gulp ahora utiliza newer para procesar archivos. Resolves #78

--- gulp-tasks/maps-modal.js
+++ gulp-tasks/maps-modal.js
@@ -1,5 +1,6 @@
 /* Imports */
 var gulp = require('gulp');
+var newer = require('gulp-newer');
 var plugins = require('gulp-load-plugins')(['gulp-*']);
 var imagemin = require('gulp-imagemin');
 var path = require('path');
@@ -8,6 +9,7 @@ var generateMapsModal = require(path.resolve(__dirname, './generateMapsModal.js'
 gulp.task('maps-modal', [], function() {
   var modelTemplate = 'src/app/home/simulator/maps-modal/maps-modal.tpl';
   gulp.src('src/game/data/map/**/*.png')
+            .pipe(newer('dist/maps-thumbnails'))
             .pipe(imagemin())
             .pipe(gulp.dest('dist/maps-thumbnails'))
             .on('error', plugins.util.log);
