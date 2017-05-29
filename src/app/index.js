d96d6ea25cfbcaf46eb49af50d4520face12000b
Lautaro Petaccio <lausuper@gmail.com>
2017-05-29 20:07:21 -0300

    Inicializacion de la aplicacion mediante las estructuras de backbone

--- src/app/index.js
+++ src/app/index.js
@@ -1,24 +1,35 @@
 "use strict";
 
-var app = app || {};
-
-this.Index = (function() {
-
-  var module = {};
-
-  module.run = function() {
-    var pageWrapperSelector = "#page-wrapper";
-    Home.initialize();
-  }
-
-  return module;
-
-})();
+this.app = app || {};
 
 $(document).ready(function() {
-  Index.run();
-});
+  app.mapsSelectorView = new app.views.MapsSelectorView({el : '#maps-modal-wrapper'});
 
-// $("#app").load(function() {
-//   Index.run();
-// });
+  app.configView = new app.views.ConfigView({el : '#home-config-wrapper'});
+
+  app.navBarView = new app.views.NavBar({el: '#home-navbar-wrapper'});
+
+  app.blocklyModel = new app.models.Blockly();
+  app.blocklyView = new app.views.Blockly({el: '#home-blockly-wrapper', model: app.blocklyModel});
+
+  app.downloadArduinoCodeModel = new app.models.FileSave({
+    fileExtension : '.ino', 
+    defaultFileName : 'arduino-code',
+    extractCode : $.proxy(app.blocklyModel.exportWorkspaceArduino, app.blocklyModel)
+  });
+  app.downloadArduinoCodeView = new app.views.FileSave({el: '#download-code-modal-wrapper', model : app.downloadArduinoCodeModel});
+
+  app.exportCodeModel = new app.models.FileSave({
+    fileExtension : '.xml', 
+    defaultFileName : 'xml-code',
+    extractCode : $.proxy(app.blocklyModel.exportWorkspaceXml, app.blocklyModel)
+  });
+  app.exportCodeView = new app.views.FileSave({el: '#export-code-modal-wrapper', model : app.exportCodeModel});
+
+  app.simulatorInterpreterModel = new app.models.SimulatorInterpreter();
+  app.simulatorModel = new app.models.Simulator({interpreter : app.simulatorInterpreterModel});
+  app.simulatorView = new app.views.Simulator({el : '#home-simulator-wrapper', model : app.simulatorModel});
+
+  app.appView = new app.views.AppView({el : '#page-wrapper'});
+
+});
