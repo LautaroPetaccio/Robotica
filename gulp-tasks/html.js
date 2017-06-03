1ac03867a0e8736e323ab24b805045ecc9d9dce2
Lautaro Petaccio <lausuper@gmail.com>
2017-06-03 11:35:07 -0300

    Modularizado el template del modal

--- gulp-tasks/html.js
+++ gulp-tasks/html.js
@@ -2,7 +2,8 @@
 var gulp = require('gulp');
 var newer = require('gulp-newer');
 var plugins = require('gulp-load-plugins')(['gulp-*']);
-const handlebars = require('gulp-compile-handlebars');
+// const handlebars = require('gulp-compile-handlebars');
+const hb = require('gulp-hb');
 const rename = require('gulp-rename');
 var path = require('path');
 const gulpHelpers = require(path.resolve(__dirname, './gulp-helpers.js'));
@@ -16,10 +17,17 @@ gulp.task('html', ['html-index']);
 gulp.task('html-index', [], function() {
   var mapsData = gulpHelpers.generateMapsData();
   return gulp.src('src/app/templates/index.hbs')
-          .pipe(handlebars({maps : mapsData}, {
-            ignorePartials: true,
-            batch: ['src/app/templates/']
+          .pipe(hb({
+              helpers: [
+                  'node_modules/handlebars-layouts/index.js'
+              ],
+              data : {maps : mapsData},
+              partials: 'src/app/templates/*.hbs'
           }))
+          // .pipe(handlebars({maps : mapsData}, {
+          //   ignorePartials: true,
+          //   batch: ['src/app/templates/']
+          // }))
           .pipe(rename({
             extname: '.html'
           }))
